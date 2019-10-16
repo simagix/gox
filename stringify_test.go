216db2daa29541d1e75ec84951b5a3fc7ca783d6
Kuei-chun Chen <simagix@gmail.com>
2019-10-16 17:25:49 -0400

    add float to test case

--- stringify_test.go
+++ stringify_test.go
@@ -8,11 +8,11 @@ import (
 )
 
 func TestStringify(t *testing.T) {
-	doc := map[string]string{"color": "Red", "style": "Truck"}
+	doc := map[string]interface{}{"color": "Red", "style": "Truck", "amount": 637.32}
 	str := Stringify(doc)
 	var v map[string]interface{}
 	json.Unmarshal([]byte(str), &v)
-	if v["color"] != doc["color"] || v["style"] != doc["style"] {
-		t.Fatal("Expected", `{"color":"Red","style":"Truck"}`, "but got", str)
+	if v["color"] != doc["color"] || v["style"] != doc["style"] || v["amount"] != doc["amount"] {
+		t.Fatal("Expected", `{"color":"Red","style":"Truck","amount":637.32}`, "but got", str)
 	}
 }
